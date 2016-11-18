43f7f82c48b4263370cd68baeaed373d794582fc
Lohith Kumar Paripati <plohithkumar@hotmail.com>
2016-11-18 14:05:06 -0800

    Add files via upload

---
+++ recipes.js
@@ -0,0 +1,39 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+module.exports = { 
+    "quick sort": "For quick sort... Best time complexity is Omega of n log n. Average is theta of n log n. Worst is Big Oh of n square.",
+	"merge sort": "For merge sort... Best time complexity is Omega of n log n. Average is theta of n log n. Worst is Big Oh of n log n.",
+	"tim sort": "For tim sort... Best time complexity is Omega of n. Average is theta of n log n. Worst is Big Oh of n log n.",
+	"heap sort": "For heap sort... Best time complexity is Omega of n log n. Average is theta of n log n. Worst is Big Oh of n log n.",
+	"bubble sort": "For bubble sort... Best time complexity is Omega of n. Average is theta of n square. Worst is Big Oh of n square.",
+	"insertion sort": "For insertion sort... Best time complexity is Omega of n. Average is theta of n square. Worst is Big Oh of n square.",
+	"selection sort": "For selection sort... Best time complexity is Omega of n square. Average is theta of n square. Worst is Big Oh of n square.",
+	"tree sort": "For tree sort... Best time complexity is Omega of n log n. Average is theta of n log n. Worst is Big Oh of n square.",
+	"shell sort": "For shell sort... Best time complexity is Omega of n log n. Average is theta of log n whole square, times n. Worst is Big Oh of log n whole square, times n.",
+	"bucket sort": "For bucket sort... Best time complexity is Omega of n plus k. Average is theta of n plus k. Worst is Big Oh of n square.",
+	"radix sort": "For radix sort... Best time complexity is Omega of n k. Average is theta of n k. Worst is Big Oh of n k.",
+	"counting sort": "For counting sort... Best time complexity is Omega of n plus k. Average is theta of n plus k. Worst is Big Oh of n plus k.",
+	"cube sort": "For cube sort... Best time complexity is Omega of n. Average is theta of n log n. Worst is Big Oh of  n log n.",
+	"array": "For array... Worst time complexity for Access is  Big oh of 1. Search is Big oh of n. Insertion is  Big oh of n. Deletion is Big oh of n.",
+	"stack": "For stack... Worst time complexity for Access is  Big oh of n. Search is Big oh of n. Insertion is  Big oh of 1. Deletion is Big oh of 1.",
+	"queue": "For queue... Worst time complexity for Access is  Big oh of n. Search is Big oh of n. Insertion is  Big oh of 1. Deletion is Big oh of 1.",
+	"singly linked list": "For singly linked list... Worst time complexity for Access is  Big oh of n. Search is Big oh of n. Insertion is  Big oh of 1. Deletion is Big oh of 1.",
+	"doubly linked list": "For doubly linked list... Worst time complexity for Access is  Big oh of n. Search is Big oh of n. Insertion is  Big oh of 1. Deletion is Big oh of 1.",
+	"skip list": "For skip list... Worst time complexity for Access is  Big oh of n. Search is Big oh of n. Insertion is  Big oh of n. Deletion is Big oh of n.",
+	"hash table": "For hash table... Worst time complexity for Search is Big oh of n. Insertion is  Big oh of n. Deletion is Big oh of n.",
+	"binary search tree": "For binary search tree... Worst time complexity for Access is  Big oh of n. Search is Big oh of n. Insertion is  Big oh of n. Deletion is Big oh of n.",
+	"cartesian tree": "For cartesian tree... Worst time complexity for Search is Big oh of n. Insertion is  Big oh of n. Deletion is Big oh of n.",
+	"b-tree": "For b-tree... Worst time complexity for Access is  Big oh of log n. Search is Big oh of log n. Insertion is  Big oh of log n. Deletion is Big oh of log n.",
+	"red-black tree": "For red black tree... Worst time complexity for Access is  Big oh of log n. Search is Big oh of log n. Insertion is  Big oh of log n. Deletion is Big oh of log n.",
+	"splay tree": "For splay tree... Worst time complexity for Search is Big oh of log n. Insertion is  Big oh of log n. Deletion is Big oh of log n.",
+	"avl tree": "For avl tree... Worst time complexity for Access is  Big oh of log n. Search is Big oh of log n. Insertion is  Big oh of log n. Deletion is Big oh of log n.",
+	"kd tree": "For kd tree... Worst time complexity for Access is  Big oh of n. Search is Big oh of n. Insertion is  Big oh of n. Deletion is Big oh of n."
+};
